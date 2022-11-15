76f52a77984491a22c28d1bc45fcaa406c4446f7
Anderson Castro <andersoncastrosantos@gmail.com>
2022-11-15 20:37:43 -0300

    Outros componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,22 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
     return(
       
       <>
 
-      <h1>Olá Mundo</h1>
+      <Header/>
+      <main>
+        <section id="sales">
+            <div className="dsmeta-container">
+                <SalesCard/>
+            </div>
 
-      <NotificationButton/>
+        </section>
+
+      </main>
 
       </>
     )
