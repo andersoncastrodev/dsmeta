eb41fe88b0f04a0c500de015a26447f3bf616f03
Anderson Castro <andersoncastrosantos@gmail.com>
2022-11-20 09:36:15 -0300

    Selecionar por Data na API

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,13 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.List;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
@@ -13,8 +18,17 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repositorySale;
 	
-	public List<Sale> findSales() {	
+	public Page<Sale> findSales(String minDate, String maxDate,Pageable pageable) {	
 		//List<Sale> listaVenda =  repositorySale.findAll();
-		return repositorySale.findAll();
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		// .minusDays( - numero de dias que vc quer.)
+		LocalDate min = minDate.equals("")? today.minusDays(365) : LocalDate.parse(minDate);
+		
+		//If ternario teste se a data esta vazio.
+		LocalDate max = maxDate.equals("")? today : LocalDate.parse(maxDate);
+		
+		return repositorySale.findSales(min, max, pageable);
 	}
 }
